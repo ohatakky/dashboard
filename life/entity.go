a1cf79527ae8ebdb55fdfdc3934359b8dcbeed5c
Akira Otaki <ohatakky@gmail.com>
2020-07-24 16:50:52 +0900

    fix

---
+++ life/entity.go
@@ -0,0 +1,4 @@
+package life
+
+// todo: サーバー起動時にglobal変数にパースしておく。GETAPIは参照するだけ
+var ()
--- pkg/life/client.go
+++ pkg/life/client.go
@@ -6,8 +6,8 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/ohatakky/dashboard/project/configs"
@@ -38,9 +38,6 @@ const (
 	timeLayout = "15:04:05"
 )
 
-// todo: サーバー起動時にglobal変数にパースしておく。GETAPIは参照するだけ
-// var
-
 type Record struct {
 	Date        time.Time     // 日付
 	Condition   int           // 調子[10]
@@ -60,49 +57,28 @@ type Record struct {
 	Vitamin     bool          // ビタミン剤
 }
 
-func (c *Client) download() error {
+func (c *Client) downloads() ([]byte, error) {
 	uFmt := "https://docs.google.com/spreadsheets/d/%s/gviz/tq?tqx=out:csv&sheet=%s"
 	u := fmt.Sprintf(uFmt, configs.E.Life.SpreadsheetID, configs.E.Life.SheetName)
 	resp, err := http.Get(u)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	f, err := os.OpenFile("./life.csv", os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	_, err = f.Write(body)
-	if err != nil {
-		return err
-	}
-	return nil
+	return body, nil
 }
 
 func (c *Client) Records() ([]Record, error) {
-	err := c.download()
+	b, err := c.downloads()
 	if err != nil {
 		return nil, err
 	}
-
-	ff, err := os.OpenFile("./life.csv", os.O_RDONLY, 0666)
-	if err != nil {
-		return nil, err
-	}
-	defer ff.Close()
-	r := csv.NewReader(ff)
-	_, err = r.Read()
-	if err != nil {
-		return nil, err
-	}
-
+	r := csv.NewReader(strings.NewReader(string(b)))
 	res := make([]Record, 0)
 	for {
 		record, err := r.Read()
@@ -113,7 +89,7 @@ func (c *Client) Records() ([]Record, error) {
 			return nil, err
 		}
 
-		// mapping to struct
+		// mappings to struct
 		tmp := Record{}
 		for i, v := range record {
 			if v == "" {
