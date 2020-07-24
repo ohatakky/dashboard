b4962a3ea8d4a574eb2c2821ec559eec4547f41a
Akira Otaki <ohatakky@gmail.com>
2020-07-25 02:03:08 +0900

    fix

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App: React.FC<{ compiler: string, framework: string }> = (props) => {
+const App: React.FC<{ compiler: string; framework: string }> = (props) => {
   return (
     <div>
       <div>{props.compiler}</div>
       <div>{props.framework}</div>
     </div>
   );
-}
+};
 
 ReactDOM.render(
   <App compiler="TypeScript" framework="React" />,
   document.getElementById("root")
 );
-
-
-
-
